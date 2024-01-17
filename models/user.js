da55e63321f59532b7d11d1de48416a61eefbae4
Elizaveta Burlakova <es.burlakova@mail.ru>
2024-01-17 22:46:21 +0300

    1

--- models/user.js
+++ models/user.js
@@ -1,33 +1,45 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
-  name: { // у пользователя есть имя — опишем требования к имени в схеме:
-    type: String, // имя — это строка
-    required: {
-      value: true, // оно должно быть у каждого пользователя, так что имя — обязательное поле
-      message: 'Поле является обязательным',
-    },
-    minlength: [2, 'Минимальная длинна 2 символа'], // минимальная длина имени — 2 символа
-    maxlength: [30, 'Максимальная длинна 30 символ'], // а максимальная — 30 символов
-  },
-  about: { // у пользователя есть occupation — опишем требования в схеме:
+  name: {
     type: String,
-    required: {
-      value: true, // оно должно быть у каждого пользователя, так что имя — обязательное поле
-      message: 'Поле является обязательным',
-    },
-    minlength: [2, 'Минимальная длинна 2 символа'], // минимальная длина имени — 2 символа
-    maxlength: [30, 'Максимальная длинна 30 символ'], // а максимальная — 30 символов
+    default: 'Жак-Ив Кусто',
+    minlength: [2, 'Минимальная длинна 2 символа'],
+    maxlength: [30, 'Максимальная длинна 30 символ'],
   },
-  avatar: { // у пользователя есть avatar — опишем требования в схеме:
+  about: {
     type: String,
-    required: true, // оно должно быть у каждого пользователя — обязательное поле
+    default: 'Исследователь',
+    minlength: [2, 'Минимальная длинна 2 символа'],
+    maxlength: [30, 'Максимальная длинна 30 символ'],
+  },
+  avatar: {
+    type: String,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
       validator(v) {
         return /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/.test(v);
       },
       message: 'Введите URL',
     },
+  },
+  email: {
+    type: String,
+    require: [true],
+    unique: [true],
+    validate: {
+      validator(email) {
+        validator.isEmail(email);
+      },
+      message: 'Введите корректный адрес электронной почты',
+    },
+  },
+  password: {
+    type: String,
+    require: [true],
+    unique: [true],
+    select: false,
   },
 }, { versionKey: false, timestamps: true });
 
